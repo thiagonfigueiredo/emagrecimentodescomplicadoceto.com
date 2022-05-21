9ce2cc28d834d391e9e2f1cc36f59b0a93a69408
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-05-21 00:19:23 -0300

    Update positive-button.js

--- assets/scripts/positive-button.js
+++ assets/scripts/positive-button.js
@@ -2,10 +2,10 @@ document.getElementById("cta1").style.display = "none";
 setTimeout(() => {
   document.getElementById("cta1").style.display = "block";
   document.getElementById("sub-header").style.display = "none";
-}, 1000);
+}, 1380000);
 
 document.getElementById("positive-button2").style.display = "none";
 setTimeout(() => {
   document.getElementById("positive-button2").style.display = "inline-block";
-}, 1000);
+}, 1380000);
 
