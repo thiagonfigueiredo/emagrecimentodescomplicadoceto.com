2359c4844a65e3ce1c89b424edb3d5ade43edc0a
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-06-04 12:32:58 -0300

    feat: update w/ antes-depois

--- assets/scripts/positive-button.js
+++ assets/scripts/positive-button.js
@@ -1,17 +1,17 @@
-document.getElementById("cta1").style.display = "none";
 setTimeout(() => {
   document.getElementById("cta1").style.display = "block";
+  document.getElementById("cta2").style.display = "block";
   document.getElementById("sub-header").style.display = "none";
-}, 1200000);
+}, 120000);
 
 document.getElementById("positive-bonus").style.display = "none";
 setTimeout(() => {
   document.getElementById("positive-bonus").style.display = "inline-block";
-}, 1200000);
+}, 120000);
 
 document.getElementById("positive-button2").style.display = "none";
 setTimeout(() => {
   document.getElementById("positive-button2").style.display = "inline-block";
-}, 1200000);
+}, 120000);
 
  
