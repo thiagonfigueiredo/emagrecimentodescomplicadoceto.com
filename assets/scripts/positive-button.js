e422331f489d5f47ea0c1babe1c68a281e90f1c2
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-06-06 08:16:17 -0300

    feat: improve cta

--- assets/scripts/positive-button.js
+++ assets/scripts/positive-button.js
@@ -1,17 +1,18 @@
 setTimeout(() => {
+  document.getElementById("cta-resultados").style.display = "flex";
   document.getElementById("cta1").style.display = "block";
   document.getElementById("cta2").style.display = "block";
   document.getElementById("sub-header").style.display = "none";
-}, 120000);
+}, 1260000);
 
 document.getElementById("positive-bonus").style.display = "none";
 setTimeout(() => {
   document.getElementById("positive-bonus").style.display = "inline-block";
-}, 120000);
+}, 1260000);
 
 document.getElementById("positive-button2").style.display = "none";
 setTimeout(() => {
   document.getElementById("positive-button2").style.display = "inline-block";
-}, 120000);
+}, 1260000);
 
  
